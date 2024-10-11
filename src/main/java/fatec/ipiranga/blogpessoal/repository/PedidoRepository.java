ece27a893dfecbf5cd7b6a579e1ca47fda0687a8
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-10 23:08:34 -0300

    cria funcoes custom para o Read (consultar) do Pedido

--- src/main/java/fatec/ipiranga/blogpessoal/repository/PedidoRepository.java
+++ src/main/java/fatec/ipiranga/blogpessoal/repository/PedidoRepository.java
@@ -3,8 +3,10 @@ package fatec.ipiranga.blogpessoal.repository;
 import fatec.ipiranga.blogpessoal.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    public List<Pedido> findAllByNumeroContainingIgnoreCase (int numero);
+	public List<Pedido> findByIdUsuario(long idUsuario);
+	public List<Pedido> findByDate(LocalDate dataPedido);
 }
