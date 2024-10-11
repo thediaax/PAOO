b451b2d3bd5e31c04be0ca3a663576763e1464eb
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-10 23:18:40 -0300

    corrige a funcao custom que pesquisa por Nome do Produto

--- src/main/java/fatec/ipiranga/blogpessoal/repository/ProdutoRepository.java
+++ src/main/java/fatec/ipiranga/blogpessoal/repository/ProdutoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    public List<Produto> findAllByProdutoContainingIgnoreCase (String nome);
+    public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
 }
